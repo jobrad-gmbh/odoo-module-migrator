686d8aba2700bcac95c819dbde59d9d09a1da474
Julian Helfferich <julian.helfferich@jobrad.org>
2024-11-13 10:47:23 +0100

    [FIX] [v14] - [v15] Add missing include

--- odoo_module_migrate/tools.py
+++ odoo_module_migrate/tools.py
@@ -2,6 +2,7 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+import ast
 import json
 import subprocess
 import re
